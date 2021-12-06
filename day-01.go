f503fe6e0423a6ab835cfd1a215b14bc37c130b9
carlmon <450101+carlmon@users.noreply.github.com>
2021-12-06 16:07:23 +0200

    Day 01

---
+++ day-01.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+)
+
+func countIncreases(inFile string) {
+	file, err := os.Open(inFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	increases := 0
+	last := math.MaxInt32
+
+	for scanner.Scan() {
+		curr, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if last < curr {
+			increases++
+		}
+		last = curr
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Increases: %d\n", increases)
+}
+
+func createSlidingWindow(inFile, outFile string) {
+	file, err := os.Open(inFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	windowFile, err := os.OpenFile(outFile, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("failed creating file: %s", err)
+	}
+
+	datawriter := bufio.NewWriter(windowFile)
+	defer windowFile.Close()
+
+	index := 0
+	windows := [3]int{0, 0, 0}
+
+	for scanner.Scan() {
+		val, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if 1 <= index {
+			windows[(index-1)%3] += val
+		}
+		if 2 <= index {
+			windows[(index-2)%3] += val
+		}
+
+		datawriter.WriteString(strconv.Itoa(windows[index%3]) + "\n")
+
+		windows[index%3] = val
+		index++
+	}
+	datawriter.Flush()
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func day01() {
+	fmt.Println("== DAY 01 ==")
+	countIncreases("./day-01-input.txt")
+	createSlidingWindow("./day-01-input.txt", "./day-01-sliding.txt")
+	countIncreases("./day-01-sliding.txt")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/carlmon/advent-of-code
+
+go 1.17
